3255cc593bec02bac189a1f720ab974070ea2fe9
John Davis <johndavisdigitalart@gmail.com>
2020-05-31 00:32:40 -0700

    Finishing importation of multiple sheets

--- generateExcel.ps1
+++ generateExcel.ps1
@@ -3,39 +3,34 @@ $excel.visible = $True
 
 $workbook = $excel.Workbooks.Add()
 
-$uregwksht = $workbook.Worksheets.Item(1)
-$uregwksht.Name = 'The name you choose'
-
 $sourcePath = 'C:\Users\davis\source\repos\PowershellExcelUtil - Files'
 
-#later: import all csv's in folder
 $csvFiles = Get-ChildItem $sourcePath -Filter *.csv
-$csvFilePaths = New-Object Collections.Generic.List[Object]
-for ($i = 0; $i -lt $csvFiles.Length; $i++) {
-	$records = Import-Csv -Path ($sourcePath + "\" + $csvFiles[$i])
-	$csvFilePaths.Add($records)
-}
-Write-Host $csvFilePaths.Length
-
-<#
-$records = Import-Csv -Path 'C:\Users\davis\source\repos\PowershellExcelUtil - Files\CSV1.csv'
-
-
-#writing headers
-for ($j = 0; $j -lt $records[0].psobject.properties.name.Length; $j++) {
-	$uregwksht.Cells.Item(1, $j + 1) = $records[0].psobject.properties.name[$j]
-}
-#filling up table
-for ($i = 0; $i -lt $records.Length; $i++) {
-	for ($j = 0; $j -lt $records[1].psobject.properties.value.Length; $j++) {
-		$uregwksht.Cells.Item($i + 2, $j + 1) = $records[$i].psobject.properties.value[$j]
+for ($k = 0; $k -lt $csvFiles.Length; $k++) {
+	#import CSV
+	$records = Import-Csv -Path ($sourcePath + "\" + $csvFiles[$k])
+	
+	#add a new worksheet
+	$uregwksht = $workbook.Worksheets.Add()
+	$uregwksht.Name = $csvFiles[$k]
+	
+	#writing headers
+	for ($j = 0; $j -lt $records[0].psobject.properties.name.Length; $j++) {
+		$uregwksht.Cells.Item(1, $j + 1) = $records[0].psobject.properties.name[$j]
 	}
+	#filling up table
+	for ($i = 0; $i -lt $records.Length; $i++) {
+		for ($j = 0; $j -lt $records[1].psobject.properties.value.Length; $j++) {
+			$uregwksht.Cells.Item($i + 2, $j + 1) = $records[$i].psobject.properties.value[$j]
+		}
+	}
+
+	#adjusting the column width so all data's properly visible
+	$usedRange = $uregwksht.UsedRange
+	$usedRange.EntireColumn.AutoFit() | Out-Null
 }
 
-#adjusting the column width so all data's properly visible
-$usedRange = $uregwksht.UsedRange
-$usedRange.EntireColumn.AutoFit() | Out-Null
+#to do: reverse order of sheets, delete last sheet
 
 $workbook.SaveAs('C:\Users\davis\source\repos\PowershellExcelUtil - Files\myExcel.xlsx')
 $excel.Quit()
-#>
